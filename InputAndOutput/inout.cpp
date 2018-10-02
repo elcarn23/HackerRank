c0f9d03012a6c597d4d3a071dcdb15438cf1d6f5
Jon Blackburn <paladin86@gmail.com>
2018-10-02 10:18:46 -0400

    Create inout.cpp
    Added solution to Input and Output challenge.

---
+++ InputAndOutput/inout.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main () {
+
+    // Three ints to hold the values.
+    int a, b, c; 
+    std::cin >> a >> b >> c;
+
+    int sum = a + b + c;
+    std::cout << sum << std::endl;
+
+    return 0;
+}
